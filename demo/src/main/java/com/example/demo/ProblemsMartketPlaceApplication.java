e07ccce922c2818307decdb5aced6ca1cfe94fff
karthick7204 <karthicklimited1234@gmail.com>
2026-02-21 22:46:00 +0530

    first commit

---
+++ demo/src/main/java/com/example/demo/ProblemsMartketPlaceApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemsMartketPlaceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProblemsMartketPlaceApplication.class, args);
+
+	}
+
+}
